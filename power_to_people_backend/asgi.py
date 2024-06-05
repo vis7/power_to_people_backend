dbca77e476baaf08c69da526f248688e85a3b156
vis <vishvajeetramanuj95@gmail.com>
2024-06-05 13:29:38 +0530

    initialized project

---
+++ power_to_people_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for power_to_people_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'power_to_people_backend.settings')
+
+application = get_asgi_application()
